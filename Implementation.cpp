87ef90b544e0262f7458ff2b0a4dbae67fab4432
Ayush-Shambharkar7 <ayushshambharkar0007@gmail.com>
2024-10-14 22:03:32 +0530

    Create Implementation.cpp

---
+++ Implementation.cpp
@@ -0,0 +1,30 @@
+/*
+  Name: Ayush Shambharkar
+  PRN: 23070123034
+  Aim: To implement linked lists.
+*/
+#include <iostream>
+using namespace std;
+
+
+    class Link{
+        public:
+        int data;
+        Link* next;
+        
+        Link(int num){
+        data = num;
+        next = NULL;
+    }
+};
+
+int main(){
+    Link* l1 = new Link(6);
+    cout<<l1 ->data<<" "<<l1->next;
+    return 0;
+}
+
+/*
+OUTPUT
+6 0
+*/
